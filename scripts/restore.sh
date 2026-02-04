2eb463a424a23ec45c3d6817fc9998c6f3b40c73
Avi Fenesh <aviarchi1994@gmail.com>
2026-02-04 19:12:17 +0000

    Restore without --cow-dump flags

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -56,7 +56,6 @@ sudo criu lazy-pages \
   --page-server \
   --address "$PRIMARY_IP" \
   --port "$CRIU_PORT" \
-  --cow-dump \
   --tcp-close \
   -v1 -o "$IMAGES_DIR/lazy-server.log" &
 LAZY_PAGES_PID=$!
@@ -71,7 +70,6 @@ sudo criu restore \
   --images-dir "$IMAGES_DIR" \
   --lazy-pages \
   --tcp-close \
-  --cow-dump \
   --skip-file-rwx-check \
   -v1 -o "$IMAGES_DIR/lazy-restore.log" &
 RESTORE_PID=$!
