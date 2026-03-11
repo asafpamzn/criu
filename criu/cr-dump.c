696102286430021b614e808572d41d6e3b2ccdfd
asafpamzn <asafp@amazon.com>
2026-03-11 15:05:29 +0200

    clear wp async

--- criu/cr-dump.c
+++ criu/cr-dump.c
@@ -2862,8 +2862,8 @@ static int cr_dump_tasks_cow_phased(pid_t pid)
 	 * In COW phased migration, the socket was stored in page_server_sk
 	 * after receiving the bulk complete ACK.
 	 */
-	pr_info("Sending dirty bitmap to replica (%u ranges, %lu pages)\n",
-		nr_dirty_ranges, nr_dirty_pages);
+	pr_info("Sending dirty bitmap to replica (%u ranges)\n",
+		nr_dirty_ranges);
 	ret = send_cow_dirty_bitmap(dirty_ranges, nr_dirty_ranges);
 	if (ret) {
 		pr_err("Failed to send dirty bitmap to replica\n");
