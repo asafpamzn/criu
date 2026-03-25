7715ee964f8b40092c6757a00ec8a5e5eded4fdb
asafpamzn <asafp@amazon.com>
2026-03-25 09:58:16 +0200

    remove remaining pages

--- criu/include/page-xfer.h
+++ criu/include/page-xfer.h
@@ -90,8 +90,7 @@ extern int send_dirty_bitmap_to_replica(int sk, u64 dst_id,
 extern int send_cow_dirty_bitmap(unsigned long *ranges, unsigned int nr_ranges);
 
 /* COW phased migration: signal replica all pages sent, can zero-fill rest */
-extern int send_all_pages_sent_signal(void);
-extern int send_all_pages_sent_signal_sk(int sk);
+extern int send_all_pages_sent_signal(int sk);
 
 /* COW phased migration: signal replica that inventory.img is ready */
 extern int send_inventory_ready_signal(void);
