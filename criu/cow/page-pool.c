4ab692e4ff007bb1c6c1e21073710397589ef5b8
asafpamzn <asafp@amazon.com>
2026-04-15 18:30:36 +0300

    Reduce debug and incease number of chunks

--- criu/cow/page-pool.c
+++ criu/cow/page-pool.c
@@ -296,8 +296,7 @@ void page_pool_put(void *page)
 
 	/* Last reference? munmap the entire chunk */
 	if (old_ref == 1) {
-		int freed_count = atomic_fetch_add(&total_chunks_freed, 1) + 1;
-		pr_err("PAGE_POOL: Freeing chunk at %p (total_freed=%d)\n", hdr, freed_count);
+		int freed_count = atomic_fetch_add(&total_chunks_freed, 1) + 1;		
 
 		/* Remove from tracking list */
 		pthread_spin_lock(&chunk_list_lock);
